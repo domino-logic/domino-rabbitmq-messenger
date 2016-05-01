93226c919024c4542610ac0845c4ae4d7cd820f6
Brice Leroy <bbrriiccee@gmail.com>
2016-04-30 22:35:01 -0700

    singularity

---
+++ messenger.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const amqp = require('amqplib/callback_api')
+
+
+class Messenger {
+  constructor (options) {
+    this.options = options || {}
+    this.assertedQueues = {}
+  }
+
+  config (options) {
+    Object.assign(this.options, options)
+  }
+
+  start (callback) {
+    const amqp_url = this.options.amqp || 'amqp://localhost';
+    amqp.connect(amqp_url, (err, conn) => {
+      this.AssertNoError(err);
+
+      conn.createChannel( (err, channel) => {
+        this.AssertNoError(err);
+        this.channel = channel;
+        callback(this)
+      });
+    });
+  }
+
+  AssertNoError (err) {
+    if (err){
+      console.error(err);
+      process.exit(1);
+    }
+  }
+
+  ack (msg) {
+    this.channel.ack(msg);
+  }
+
+  consume (queue, callback) {
+    this.assertQueue(queue);
+    this.channel.consume(queue, (msg) => {
+      msg.content = JSON.parse(msg.content)
+      callback(msg.content);
+      this.ack(msg);
+    })
+  }
+
+  publish (queue, json) {
+    this.assertQueue(queue);
+
+    const message = new Buffer(JSON.stringify(json));
+
+    this.channel.sendToQueue(
+      queue,
+      message,
+      {persistent: true}
+    );
+
+    console.log(`Published message to ${queue}: `, json.payload);
+  }
+
+  assertQueue (queue) {
+    if (queue in this.assertedQueues)
+      return
+
+    this.channel.assertQueue(queue, {durable: true});
+    this.assertedQueues[queue] = true;
+  }
+}
+
+
+
+module.exports = Messenger
